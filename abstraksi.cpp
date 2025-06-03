a5335eb2f1b153ece4023992c1658d05ed337ad8
paw2kk <wndxakk@gmail.com>
2025-06-03 10:40:08 +0700

    membuat class abstraksi class yang berisi string x,y,fungsi setXY yang berfungsi mendefinisikan x dan y ke a dan b,dan fungsi display untuk mengerluarkan variabel x dan y

---
+++ abstraksi.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+class AbstraksiKlas {
+    private: string x,y;
+
+    public:
+    //method untuk mengisi nilai
+    //private member
+    void setXY(string a, string b){
+    x = a;
+    y = b;
+    }
+
+    //menampilkan display
+    void display(){
+        cout << "x = " << x << endl;
+        cout << "y = " << y << endl;
+    }
+};
