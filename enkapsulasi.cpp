7dee1d2ad28a1f8d1d4f46cc0a173301487fff71
paw2kk <wndxakk@gmail.com>
2025-06-03 10:46:50 +0700

    membuat class remotelampu yang berisi fungsi sersaklarno yang bertujuan mengetahui saklar yang hidup

---
+++ enkapsulasi.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+class remoteLampu{
+    private:
+    string saklarNo[10];
+    public:
+    void setSakalrNo(int i, string value) {
+        saklarNo[i] = value;
+        }
+         string getSaklarNo(int i){
+                return saklarNo[i];
+            }
+    
+};
+
+int main(){
+
+}
