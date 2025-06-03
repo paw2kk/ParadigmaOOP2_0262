4ed47a2d975868486d0f0af4b20be0fb83f4d6e1
paw2kk <wndxakk@gmail.com>
2025-06-03 10:21:04 +0700

    membuat class pekerja dengan variabel umur dan koneksi dengan class orang

--- multiwaris.cpp
+++ multiwaris.cpp
@@ -12,6 +12,16 @@ class orang {
     }
 };
 
+class pekerja : virtual public orang {
+    public :
+
+    pekerja(int pUmur) :
+    orang(pUmur)
+    {
+        cout << "pekerja dibuat\n" << endl;
+    }
+};
+
 int main(){
 
 }
