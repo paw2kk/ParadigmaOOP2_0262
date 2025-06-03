1baed04a6224f3330ccb51b23a9b22f563862ffb
paw2kk <wndxakk@gmail.com>
2025-06-03 10:16:37 +0700

    membuat class orang dan variabel umur

---
+++ multiwaris.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+class orang {
+    public :
+    int umur;
+
+    orang(int pUmur) :
+    umur(pUmur)
+    {
+        cout << "orang dibuat dengan umur " << umur << "\n" << endl;
+    }
+};
+
+int main(){
+
+}
